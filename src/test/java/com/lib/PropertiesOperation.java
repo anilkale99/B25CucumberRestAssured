c9ab9036473d86f8646d171f095ece53be6de67a
Anil Kale <anilkale.99@gmail.com>
2024-03-24 08:46:11 +0530

    latest

--- src/test/java/com/lib/PropertiesOperation.java
+++ src/test/java/com/lib/PropertiesOperation.java
@@ -4,9 +4,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
-
-	
-
 public class PropertiesOperation {
 	
 	public static String filename1= "src/test/resources/config/config.properties";
